7051b123da16971962e47fbf119650f172ace0ff
Student <example@example.com>
2019-01-08 06:55:10 -0800

    reinstall and configure Jasmine & Karma.

--- karma.conf.js
+++ karma.conf.js
@@ -30,8 +30,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/*.js': ['webpack'],
-      'spec/*spec.js': ['webpack']
+      'src/*.js': ['webpack', 'sourcemap'],
+      'spec/*spec.js': ['webpack', 'sourcemap']
     },
 
     plugins: [
