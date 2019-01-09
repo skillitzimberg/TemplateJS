f32a5d39a50835084f3d71555b41b5ce52d2f726
Student <example@example.com>
2019-01-08 16:41:45 -0800

    fix typos and dependency problems.

--- src/TemplateJS.js
+++
@@ -1,3 +0,0 @@
-export function TemplateJS(greeting) {
-  this.greeting = greeting;
-}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import $ from 'jquery';
-// import { TemplateJS } from './TemplateJS';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  function sayHello() {
-    var greeting = 'Hello from TemplateJS!';
-    alert(greeting);
-  }
-  sayHello();
+
 });
